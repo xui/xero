0bf4628625e09a7c16451098e6fa7612a7e49a46
Rylan Barnes <rylan@barn.es>
2023-10-05 16:57:49 -0500

    I dunno.  It's all a blur.

--- Demos/Website/UI/Views/Cards/JavaScript/JavaScript.cs
+++ Demos/Website/UI/Views/Cards/JavaScript/JavaScript.cs
@@ -2,35 +2,37 @@ partial class UI
 {
     View JavaScript(ViewModel vm) => $"""
         <section>
-            <div>
+            <div class="card-hero">
                 <button onclick="{Increment}">Clicks: {vm.Count}</button>
             </div>
-            <h2>
-                Simplify
-            </h2>
-            <h1>
-                <em>Zero</em> JavaScript
-            </h1>
-            <p>
-                I love JavaScript — been using it with ❤️‍🩹 since 1999. But JavaScript has a
-                payload problem.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div class="card-content">
+                <h2 class="tag">
+                    Simplify
+                </h2>
+                <h1>
+                    <em>Zero</em> JavaScript
+                </h1>
+                <p>
+                    I love JavaScript — been using it with ❤️‍🩹 since 1999. But JavaScript has a
+                    payload problem.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
         </section>
         """;
 
