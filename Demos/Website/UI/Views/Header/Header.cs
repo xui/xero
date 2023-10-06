88f497fa9292857b0cfc69437df405525415f769
Rylan Barnes <rylan@barn.es>
2023-10-06 17:33:27 -0500

    Poke

--- Demos/Website/UI/Views/Header/Header.cs
+++ Demos/Website/UI/Views/Header/Header.cs
@@ -1,13 +1,29 @@
 partial class UI
 {
     View Header() => $"""
-        <section id="header">
-            <img src="xero_logo.svg" />
-            <h1>Build realtime web apps with <strong>zero</strong> JavaScript</h1>
-            <h2>
-                What would web development look like if it were reconsidered from first
-                principles by mobile developers?
-            </h2>
+        <section id="header" class="hstack">
+            <div class="vstack">
+                <img src="xero_logo.svg" />
+            </div>
+            <div class="vstack">
+                <h1>
+                    Build realtime web apps with <strong>zero</strong> JavaScript
+                </h1>
+                <h2>
+                    Xero treats your web browser like a thin client.
+                </h2>
+                <ul>
+                    <li>
+                        HTML is always rendered server-side
+                    </li>
+                    <li>
+                        DOM events piped to the server via WebSocket
+                    </li>
+                    <li>
+                        Xero reacts to render-deltas with DOM mutations
+                    </li>
+                </ul>
+            </div>
         </section>
-        """;
+""";
 }
