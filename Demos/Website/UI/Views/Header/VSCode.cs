e38ff289c230b3e762694f72df6a9fc76ed040b9
Rylan Barnes <rylan@barn.es>
2023-10-06 15:38:53 -0500

    VSCode progress...

--- Demos/Website/UI/Views/Header/VSCode.cs
+++ Demos/Website/UI/Views/Header/VSCode.cs
@@ -1,24 +1,33 @@
 partial class UI
 {
-    View VSCode() => $$"""
-        <section>
-            <pre>
-                <code>
-                    partial class UI
-                    {
-                        View VSCode(ViewModel viewModel) => $"
-                            <button onclick="{Increment}">
-                                Clicks: {viewModel.Count}
-                            </button>
-                        ";
-
-                        void Increment(Context ctx)
-                        {
-                            ctx.ViewModel.Count++;
-                        }
-                    }
-                </code>
-            </pre>
+    View VSCode() => $"""
+        <section class="vscode">
+            <div class="sidebar">
+                <details open>
+                    <summary>Views</summary>
+                    {File("Child.cs")}
+                    {File("Footer.cs")}
+                    {File("Header.cs")}
+                    {File("Header.cs")}
+                    {File("Header.cs")}
+                    {File("Header.cs")}
+                    {File("Header.cs")}
+                </details>
+                {File("UI.cs")}
+                {File("ViewModel.cs")}
+            </div>
+            <div class="code">
+                <img src="/img/vscode_logo.svg" />
+                <p>
+                    VS Code &#x2764; C#
+                </p>
+            </div>
         </section>
+        """;
+
+    View File(string name) => $"""
+        <label for="{name}">
+            <input type="radio" name="file" id="{name}">{name}</input>
+        </label>
         """;
 }
