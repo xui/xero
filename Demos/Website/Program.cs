2657ee6889fcbfd0365618b67740be3e0fc4acfd
Rylan Barnes <rylan@barn.es>
2023-11-08 14:11:39 -0600

    I want to keep this in here for now.  Will remove later.

--- Demos/Website/Program.cs
+++ Demos/Website/Program.cs
@@ -2,6 +2,7 @@ global using Xero;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddXero();
+// builder.Services.AddLogging(c => c.ClearProviders()); // TODO: Remove
 
 var app = builder.Build();
 app.MapUI("/", new UI());
