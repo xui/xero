c073c05714dc0c61511fafa160803897b365038d
Rylan Barnes <rylan@barn.es>
2023-11-10 08:19:08 -0600

    Do not need

--- XeroSourceGenerators/LiveAttribute.cs
+++ XeroSourceGenerators/LiveAttribute.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 namespace Xero
 {
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
-    [Conditional("LiveGenerator_DEBUG")]
     public sealed class LiveAttribute : Attribute
     {
         public LiveAttribute()
