7065333337c0d11c54f4c2cae7945669903036cf
Rylan Barnes <rylan@barn.es>
2023-11-10 10:20:39 -0600

    Punting the concept of deeply nested objects to another task.

--- Demos/Website/UI/ViewModel.New.cs
+++ Demos/Website/UI/ViewModel.New.cs
@@ -5,13 +5,5 @@ partial class ViewModel
         Name = "Rylan Barnes",
         Count = 0,
         Color = "purple",
-        Sub = new()
-        {
-            First = "Rylan",
-            Last = "Barnes",
-            Email = "rylan@barn.es",
-            Password = "1234",
-            Quantity = 4,
-        }
     };
 }
